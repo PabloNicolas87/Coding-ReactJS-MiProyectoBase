6a03aa325eafc0a7d9cf485bd28f9e1d31f24614
PabloNicolas87 <gironepablo@gmail.com>
2025-07-15 19:28:30 -0300

    FinalTres

--- scripts/init-project.sh
+++ scripts/init-project.sh
@@ -38,10 +38,15 @@ sed -i -E "s/\"version\": *\"[^\"]+\"/\"version\": \"$VERSION\"/"    "$TARGET_DI
 rm -f "$TARGET_DIR/package-lock.json"
 
 # 3) Reemplazar placeholders en resto de ficheros relevantes
+# 2c) Reescribir name y version en package.json, eliminar lock viejo
+#    y así no arrastrar los valores del proyecto base
+sed -i -E "s/\"name\": *\"[^\"]+\"/\"name\": \"$PROJECT_NAME\"/" "$TARGET_DIR/package.json"
+sed -i -E "s/\"version\": *\"[^\"]+\"/\"version\": \"$VERSION\"/"    "$TARGET_DIR/package.json"
+rm -f "$TARGET_DIR/package-lock.json"
+
+# 3) Reemplazar placeholders en resto de ficheros relevantes
 find "$TARGET_DIR" -type f \
   \( \
-    -name "*.json" -o \
-    -name "package-lock.json" -o \
     -name "*.yml"  -o \
     -name "*.md"   -o \
     -name ".gitignore" -o \
